1fbdd4bc507a8d7d5e792d6f73f3ff70e831b8ce
Adwaith R Krishna <adwaithrk19@gmail.com>
2018-12-20 20:05:53 +0530

    admin view mostly completely 2

--- app/assets/javascripts/admin_interface.js
+++ app/assets/javascripts/admin_interface.js
@@ -13,19 +13,19 @@ $(document).ready(function () {
   $('select[name=question_type]').change(function () {
     var selected_option = $(this).val();
     debugger;
+    $('.Answer_Options').empty().show()
+    $('.save-question-button').remove()
 
     if (selected_option == "Text") {
-      $('.Answer_Options').empty().show()
+
       debugger;
       $('.Answer_Options').append("<div><input type='text' value='' placeholder='text answer'  class='Answer_options_item' readonly /></div>")
     }
     else {
-      $('.Answer_Options').empty().show()
       $('.Answer_Options').append("<div id= option-" + option_count + "><input type='text' value='' placeholder='Option 1'  class='Answer_options_item'/><span color='blue' class='remove-option' data-remove-option-id=" + option_count + ">remove-option</span></div>")
       $('.Answer_Options').append("<div class='Add_Answer_option_div'><font color='blue' class='Add_Answer_options_item'>Add option</font></div>")
     }
-
-
+    $('.question-box').append("<button type='button' class='btn btn-primary save-question-button'>save question</button>")
     // update DOM based on selectedCar
     // console.log(selectedCar)
   });
@@ -34,15 +34,18 @@ $(document).ready(function () {
     option_count++
     $('.Add_Answer_option_div').remove()
     $('.Answer_Options').append("<div id='option-" + option_count + "'><input type='text' value='' placeholder='Option " + option_count + "'  class='Answer_options_item'/><span color='blue' class='remove-option' data-remove-option-id=" + option_count + ">remove-option</span></div>")
-    $("#option-" + option_count + "").append("<div class='Add_Answer_option_div'><font color='blue' class='Add_Answer_options_item'>Add option</font></div>");
+    $("#option-" + option_count + "").after("<div class='Add_Answer_option_div'><font color='blue' class='Add_Answer_options_item'>Add option</font></div>");
     debugger
   })
 
   $('body').on("click", ".remove-option", function () {
-    $this.get
+    option_attr = this.getAttribute("data-remove-option-id");
+    $("#option-" + option_attr + "").remove()
+    option_count--
+    console.log(option_count);
   })
 
-
-
 })
+
+$()
 
